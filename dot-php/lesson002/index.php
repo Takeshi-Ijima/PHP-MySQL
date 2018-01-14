d4c66bf57bcd9e47baa93ba2d558829e4c4115ee
Takeshi Ijima <takeshi.ijima.www@gmail.com>
2018-01-15 01:31:30 +0900

    定数

---
+++ dot-php/lesson002/index.php
@@ -0,0 +1,12 @@
+<?php
+    define("DB_NAME", "localhost");
+
+    echo DB_NAME;
+
+    // エラー
+    // echo DB_NAME = "hoge";
+
+    var_dump(__LINE__);
+    var_dump(__FILE__);
+    var_dump(__DIR__);
+ ?>
