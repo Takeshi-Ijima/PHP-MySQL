2ff86b5967f9b1df6d32ae47cd3867aae6b7b5a0
Takeshi Ijima <takeshi.ijima.www@gmail.com>
2018-01-15 00:09:57 +0900

    ドットインストールphp

---
+++ dot-php/lesson001/index.php
@@ -0,0 +1,16 @@
+<?php
+    $name = "php";
+ ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHPドットインストール</title>
+    </head>
+    <body>
+        <?php
+            // echo "Hi PHP";
+            echo "Hi {$name}" . "<br>";
+            var_dump($name);
+        ?>
+    </body>
+</html>
