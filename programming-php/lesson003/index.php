1661d5426b435782dbb2e438070e32ce205c577b
Takeshi Ijima <takeshi.ijima.www@gmail.com>
2018-01-14 10:56:25 +0900

    functionについて

---
+++ programming-php/lesson003/index.php
@@ -0,0 +1,23 @@
+<?php
+    $length = strlen("PHP");
+    // echo $length;
+
+    function inhtml(){
+        ?>
+        <h1>PHP関数</h1>
+        <?php
+    }
+    // inhtml();
+
+    function FullName($fname, $lname){
+        return $fname . $lname;
+    }
+    $fname = "小峰";
+    $lname = "周平";
+
+    // echo fullname($fname, $lname);
+
+
+
+
+ ?>
